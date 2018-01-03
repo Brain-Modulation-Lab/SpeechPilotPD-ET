1aa318ef75525eab3ac16fb56b184c3b6e4e9fb7
Peter Jones <pwjones76@gmail.com>
2018-01-03 13:49:56 -0500

    intermediate

--- setDirectories.m
+++ setDirectories.m
@@ -1,6 +1,6 @@
 % Just setup the directories for data, etc
 if ispc
-    docDir = '\\136.142.16.9\Nexus\Users\pwjones\code\ET-PD project';
+    docDir = '\\136.142.16.9\Nexus\Users\pwjones\docs\ET-PD project';
     codeDir = '\\136.142.16.9\Nexus\Users\pwjones\code\SpeechPilotPD-ET';
     datadir='\\136.142.16.9\Nexus\Electrophysiology_Data\DBS_Intraop_Recordings';
     figDir = '\\136.142.16.9\Nexus\Users\pwjones\figureDump';
