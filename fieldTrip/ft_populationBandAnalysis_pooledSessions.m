29507dbfb7ea09be8dad54cf9cf377c0bcbc77e9
Peter Jones <pwjones76@gmail.com>
2020-05-05 14:41:36 -0400

    New version of populationBandAnalysis that pools over multiple sessions for electrode data

---
+++ fieldTrip/ft_populationBandAnalysis_pooledSessions.m
@@ -0,0 +1,50 @@
+% Script that assembles the band-passed data across freqencies, locations
+% This version does so per unique electrode position rather than leaving multiple sessions per electrode separate
+
+setDirectories; %platform specific locations
+groups = {'PD', 'ET'};
+ids = {'DBS2*', 'DBS4*'};
+%freqs={'hgamma','broadbandGamma','gamma','hgamma','beta1','beta2','delta','theta','alpha'};
+freqs={'hgamma','gamma','beta1','beta2','delta','theta','alpha'};
+locs = {'M1', 'S1', 'STG'};
+dd = [savedDataDir filesep 'population'];
+
+plotb = 0;
+if plotb %Plot the population results
+    
+end
+
+for gg=1:2
+    clear freq; %this is the population data structure
+    for ff=1:length(freqs)
+        disp(['Processing ' freqs{ff}]);
+        load([dd filesep 'Pop_ecog_' groups{gg} '_' freqs{ff} '.mat']);
+        for ll=1:length(locs)
+            if strcmp(freqs{ff}, 'hgamma') %Include in analysis based on hgamma response
+                gammasig = arrayfun(@(x) x.sig_h', popData,'UniformOutput', 0);
+            end
+            locmatch = arrayfun(@(x) strcmpi(x.electrodeLoc, locs{ll}), popData, 'UniformOutput', 0);
+            include =  cellfun(@(x,y) x & y, gammasig, locmatch, 'UniformOutput', 0);
+            include_session = cellfun(@(x) sum(x)>0, include); %Are any electrodes from session included
+            perSessionZ = cell2mat(arrayfun(@(x,y) x.meanz(:,y{1}), popData, include, 'UniformOutput', 0));
+            %get the speech timing means
+            e_cell = {popData(include_session).epoch};
+            bad_trial_cell = {popData(include_session).badtrial_final};
+            used_trial_cell = cellfun(@(x,y) setdiff(1:size(x,1), y), e_cell, bad_trial_cell, 'UniformOutput', 0);
+            stim_timing = cellfun(@(x,y) mean(x.stimulus_starts(y) - x.onset_word(y)), e_cell, used_trial_cell);
+            offset_timing = cellfun(@(x,y) mean(x.offset_word(y) - x.onset_word(y)), e_cell, used_trial_cell);
+            
+            loc(ll).perSessionZ = perSessionZ;
+            loc(ll).time = popData(1).time;
+            loc(ll).mean_stimOnset = mean(stim_timing);
+            loc(ll).mean_speechOffset = mean(offset_timing);
+            loc(ll).include = include;  
+        end
+        freq(ff).loc = loc;
+    end
+    eval(['summaryData.' groups{gg} '.freq=freq;']);
+    summaryData.freq_labels = freqs;
+    summaryData.loc_labels = locs;
+end
+
+save(fullfile(savedDataDir, 'population', 'summaryData_EcoG_BandAnalysis.mat'), 'summaryData', '-v7.3');
